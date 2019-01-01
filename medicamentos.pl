f763d7d81c3f5eacea7ee2ef0965098ad0869edf
Miguel Contreras <migueldevelop@gmail.com>
2019-01-01 11:39:14 -0400

    Initial commit

---
+++ medicamentos.pl
@@ -0,0 +1,390 @@
+﻿% Autor: Miguel Contreras
+% C.I. 24401758
+% Taller de Lenguajes de Programación
+% Sección 01
+
+
+medicamentos('VIH/SIDA', [
+    medicamento('ATRIPLA',
+        ['EFAVIRENZ',
+        'EMTRICITABINE',
+        'TENOFOVIR'],
+        [],
+        ['HEPATITIS',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'ENFERMEDAD DEL CORAZON',
+        'HIPERTENSION','DIABETES',
+        'COLESTEROL',
+        'HLA-B*5701',
+        'EMBARAZO']),
+    medicamento('COMPLERA',
+        ['EMTRICITABINE',
+        'RILLPIVIRINE',
+        'TENOFOVIR'],
+	[],
+        ['ENFERMEDAD DEL RIÑON',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD MENTAL',
+        'SINDROME QT',
+        'OSTEOPENIA']),
+    medicamento('EPIVIR',
+        ['LAMIVUDINE'],
+	[],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'PANCREATITIS',
+        'LAMIVUDINE',
+        'MENOR A 2 AÑOS']),
+    medicamento('EPZICON',
+        ['ABACAVIR',
+        'LAMIVUDINE'],
+	[],
+        ['HEPATITIS C',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'ENFERMEDAD DEL CORAZON',
+        'HIPERTENSION',
+        'FUMADOR',
+        'DIABETES',
+        'COLESTEROL ALTO',
+        'HLA-B*5701']),
+    medicamento('INTELENCE',
+        ['ETRAVIRINE'],
+        ['DELAVIRDINE',
+        'EFAVIRENZ',
+        'NEVIRAPINE',
+        'RIFABUTIN',
+        'RIFAPENTINE',
+        'RIFAMPIN'],
+        ['EMBARAZO']),
+    medicamento('ISENTRESS',
+        ['RALTEGRAVIR'],
+	[],
+        ['DESORDEN MUSCULAR',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD MENTAL',
+        'DEPRESION']),
+    medicamento('KALETRA',
+        ['LOPINAVIR',
+        'RITONAVIR'],
+        ['ALFUZOSIN',
+        'PIMOZIDE',
+        'RIFAMPIN',
+        'LOVASTATIN',
+        'SIMVASTATIN',
+        'MIDAZOLAM',
+        'TRIAZOLAM',
+        'SILDENAFIL'],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL CORAZON',
+        'DESORDEN DE RITMO CARDIACO',
+        'SINDROME DE QT LARGO',
+        'PROBLEMA DE PANCREAS',
+        'DIABETES',
+        'BAJO POTASIO',
+        'DESORDEN DE SANGRAMIENTO',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS']),
+    medicamento('LEXIVA',
+        ['FOSAMPRENAVIR'],
+	[],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'DIABETES',
+        'DESORDEN DE SANGRAMIENTO',
+        'ENFERMEDAD DEL CORAZON',
+        'ATAQUE AL CORAZON',
+        'ALERGIA A SULFA',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS']),
+    medicamento('NORVIR',
+        ['RITONAVIR'],
+        ['ALFUZOSIN',
+        'CISAPRIDE',
+        'PIMOZIDE',
+        'SILDENAFIL',
+        'VORICONAZOLE'],
+        ['MEDICINA COLESTEROL ALTO',
+        'MEDICINA RITMO CARDIACO',
+        'SEDATIVO',
+        'ENFERMEDAD DEL HIGADO',
+        'DESORDEN DE PANCREAS',
+        'DIABETES',
+        'ENFERMEDAD DEL CORAZON',
+        'DESORDEN DE RITMO CARDIACO',
+        'DESORDEN DE SANGRADO',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS']),
+    medicamento('PREZISTA',
+        ['DARUNAVIR'],
+        ['ALFUZOSIN',
+        'CISAPRIDE',
+        'PIMOZIDE',
+        'LOVASTATIN',
+        'SIMVASTATIN',
+        'RIFAMPIN',
+        'SILDENAFIL',
+        'TRIAZOLAM'],
+        ['ENFERMEDAD DEL HIGADO',
+        'DIABETES',
+        'DESORDEN DE SANGRADO',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS',
+        'ALERGIA A SULFA']),
+    medicamento('REYATAZ',
+        ['ATAZANAVIR'],
+	['ABACAVIR',
+        'DIDANOSINE',
+        'EMTRICITABINE',
+        'LAMIVUDINE',
+        'TENOFOVIR',
+        'ZALCITABINE',
+        'ZIDOVUDINE'],
+	['ENFERMEDAD DEL RIÑON',
+	'DIABETES']),
+    medicamento('STRIBILD',
+        ['COBICISTAT',
+        'ELVITEGRAVIR',
+        'EMTRICITABINE',
+        'TENOFOVIR'],
+        ['ALFUZOSIN',
+        'LOVASTATIN',
+        'SIMVASTATIN',
+        'MIDAZOLAM',
+        'TRIAZOLAM',
+        'PIMOZIDE',
+        'RIFAMPIN',
+        'SILDENAFIL'],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'OSTEOPENIA',
+        'HEPATITIS B',
+        'SOBREPESO',
+        'EMBARAZO']),
+    medicamento('SUSTIVA',
+        ['EFAVIRENZ'],
+        ['MIDAZOLAM'],
+        ['ENFERMEDAD DEL HIGADO',
+        'EPILEPSIA',
+        'COLVULSIONES',
+        'ENFERMEDAD MENTAL',
+        'ANTIPSICOTICO',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS',
+        'EMBARAZO']),
+    medicamento('TRIZIVIR',
+        ['ABACAVIR',
+        'LAMIVUDINE'],
+	[],
+        ['HEPATITIS B',
+        'HEPATITIS C',
+        'SUPRESION DE MEDULA OSEA',
+        'ENFERMEDAD DEL RIÑON',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL CORAZON',
+        'HIPERTENSION',
+        'EMBARAZO']),
+    medicamento('TRUVADA',
+        ['EMTRICITABINE',
+        'TENOFOVIR'],
+	[],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'HEPATITIS B']),
+    medicamento('VIRACEPT',
+        ['NELFINAVIR'],
+        ['ALFUZOSIN',
+        'PIMOZIDE',
+        'RIFAMPIN',
+        'SILDENAFIL',
+        'LOVASTATIN',
+        'SIMVASTATIN',
+        'AMIODARONE',
+        'TRIAZOLAM'],
+        ['ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'DIABETES',
+        'DESORDEN DE SANGRAMIENTO',
+        'COLESTEROL ALTO',
+        'TRIGLICERIDOS ALTOS']),
+    medicamento('VIRAMUNE',
+        ['NEVIRAPINE'],
+        ['ARTEMETHER',
+        'LUMEFANTRINE',
+        'BUSTINIB',
+        'CABAZITAXEL',
+        'DELAVIRDINE',
+        'EFAVIRENZ',
+        'ETRAVIRINE',
+        'ATAZANAVIR',
+        'FOSAMPRENAVIR',
+        'ITRACONAZOLE',
+        'KETOCONAZOLE',
+        'LURASIDONE',
+        'RIFAMPIN',
+        'RILLPIVIRINE'],
+	['ENFERMEDAD DEL HIGADO']),
+    medicamento('VIREAD',
+        ['TENOFOVIR'],
+        ['ABACAVIR',
+        'DIDANOSINE',
+        'EMTRICITABINE',
+        'LAMIVUDINE',
+        'TENOFOVIR',
+        'ZALCITABINE',
+        'ZIDOVUDINE'],
+	['EMBARAZO',
+	'ENFERMEDAD DEL RIÑON',
+	'DIABETES']),
+    medicamento('ZERIT',
+        ['STAVUDINE'],
+	['ABACAVIR'],
+        ['EMBARAZO',
+        'ENFERMEDAD DEL CORAZON',
+        'DIABETES',
+        'HEPATITIS B',
+        'COLESTEROL ALTO',
+        'FUMADOR',
+        'BEBEDOR',
+        'SOBREPESO',
+        'HLA-B*5701']),
+    medicamento('ZIAGEN',
+        ['ABACAVIR'],
+	[],
+        ['MENOR A 2 AÑOS',
+        'ENFERMEDAD DEL HIGADO',
+        'ENFERMEDAD DEL RIÑON',
+        'BAJA DENSIDAD MINERAL DE LOS HUESOS'])
+]).
+
+
+% ========== RELACIONES AUXILIARES (USO INTERNO DEL PROGRAMA) ==========
+
+% Determina si una lista es subconjunto de otra lista.
+subconjunto([],[]).
+subconjunto([X|L1],[X|L2]) :-
+	subconjunto(L1,L2).
+subconjunto([_|L1],L2) :-
+	subconjunto(L1,L2).
+
+
+% ========== RELACIONES PARA USO DEL MÉDICO ==========
+
+% Muestra una lista de enfermedades que pueden ser consultadas.
+enfermedades_medicables() :-
+	medicamentos(X,_),
+	write(X), nl, false.
+
+% Recibe el nombre de un medicamento de marca y muestra una lista
+% de medicamentos genericos relacionados.
+%
+% PARÁMETROS:
+% M -> Cadena de caracteres que representa el nombre del medicamento
+%      de marca.
+nombres_genericos(M) :-
+	medicamentos(_,L1),
+	member(medicamento(M,L2,_,_),L1),
+	member(G,L2),
+	write(G),nl,false.
+
+% Recibe el nombre de un medicamento genérico y muestra una lista
+% de medicamentos de marca relacionados.
+%
+% PARAMETROS:
+% G -> Cadena de caracteres que representa el nombre del medicamento
+%      genérico.
+nombres_marcas(G) :-
+	medicamentos(_,L1),
+	member(medicamento(M,L2,_,_),L1),
+	member(G,L2),
+	write(M),nl,false.
+
+% Recibe el nombre de la enfermedad que padece el paciente y muestra
+% todos las marcas de los medicamentos disponibles.
+%
+% PARÁMETROS:
+% E -> Cadena de caracteres que representa la enfermedad.
+nombres_todos_marcas(E) :-
+	medicamentos(E,L1),
+	member(medicamento(M,_,_,_),L1),
+	write(M),nl,false.
+
+% Recibe el nombre de la enfermedad que padece el paciente y muestra
+% todos los nombres genéricos de los medicamentos disponibles.
+%
+% PARÁMETROS:
+% E -> Cadena de caracteres que representa la enfermedad.
+
+nombres_todos_genericos(E) :-
+	medicamentos(E,L1),
+	member(medicamento(_,G,_,_),L1),
+	member(X,G),
+	write(X),nl,false.
+
+
+
+% Recibe la enfermedad que padece el paciente, una lista de medicamentos
+% que toma el paciente y una lista de condiciones médicas. Muestra los
+% medicamentos recomendados para tal paciente.
+%
+% PARÁMETROS:
+% E -> Cadena de caracteres que representa la enfermedad que padece el
+%      paciente.
+% M -> Lista de cadenas de caracteres que representan las medicinas que
+%      toma el paciente.
+% C -> Lista de cadenas de caracteres que representan las condiciones
+%      médicas que presenta el paciente.
+medicamentos_recomendados(E,M,C) :-
+	medicamentos(E,L1),
+	member(medicamento(X,_,L2,L3),L1),
+	not(subconjunto(M,L2)),
+	not(subconjunto(C,L3)),
+        write(X),nl,false.
+
+% Recibe la enfermedad que padece el paciente, una lista de medicamentos
+% que toma el paciente y una lista de condiciones médicas. Muestra los
+% medicamentos no recomendados para tal paciente.
+%
+% PARÁMETROS:
+% E -> Cadena de caracteres que representa la enfermedad que padece el
+%      paciente.
+% M -> Lista de cadenas de caracteres que representan las medicinas que
+%      toma el paciente.
+% C -> Lista de cadenas de caracteres que representan las condiciones
+%      médicas que presenta el paciente.
+medicamentos_no_recomendados(E,M,C) :-
+	medicamentos(E,L1),
+	member(medicamento(X,_,L2,L3),L1),
+	(
+	    subconjunto(M,L2);
+	    subconjunto(C,L3)
+	),
+        write(X),nl,false.
+
+% Recibe la marca de un medicamento y muestra aquellos pacientes que
+% están en riesgo por usar algún medicamento extra.
+%
+% PARÁMETROS:
+% M -> Cadena de caracteres que representa la marca del medicamento.
+pacientes_en_riesgo_por_medicamento(M) :-
+	medicamentos(_,L1),
+	member(medicamento(M,_,L2,_),L1),
+	write('Pacientes que usan:'),nl,
+	member(L4,L2),
+	write(L4),nl,false.
+
+% Recibe la marca de un medicamento y muestra aquellos pacientes que
+% están en riesgo por tener alguna condición especial (enfermedad,
+% edad, etc).
+%
+% PARÁMETROS:
+% M -> Cadena de caracteres que representa la marca del medicamento.
+
+pacientes_en_riesgo_por_condicion(M) :-
+	medicamentos(_,L1),
+	member(medicamento(M,_,_,L2),L1),
+	write('Pacientes que tienen las siguientes condiciones:'),nl,
+	member(L4,L2),
+	write(L4),nl,false.
+
